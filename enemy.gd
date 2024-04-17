0d40c15fc355c98608d845d19ceaca9f872f0688
avelitj12 <avelitj12@gmail.com>
2024-04-17 16:05:39 -0600

    Melee range attacking for player and enemies
    Add close and melee range indicator models
    
    Melee range includes close range
    
     When enemy in "melee range", allow player to press click to attack an enemy once per second, deleting them
    
    Show player attack refresh meter to indicate when attacking is ready again
    
    Enemies attack player after 1 second of time being in melee range
    
    If player hit, lose (reset game)

--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,14 @@
 extends CharacterBody3D
 
-signal hit_player
-
 @export var target : Player
 
-const SPEED = 3.0
+const SPEED = 1.2
 const JUMP_VELOCITY = 4.5
+const CLOSE_RANGE = .75
+const MELEE_RANGE = 1.5
 
 var close_targets: Array
+var melee_targets: Array
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -17,28 +18,43 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	look_at(target.global_position, Vector3.UP, true)
-	
-	if close_targets.find(target) != -1:
-		attack(target)
+	look_at(Vector3(target.global_position.x, 0, target.global_position.z), Vector3.UP, true)
+	if target in melee_targets:
+		if $Abilities/Attack/AttackCooldownTimer.is_stopped():
+			$Abilities/Attack/AttackCooldownTimer.start()
 	else:
-		var direction = (target.transform.origin - transform.origin).normalized()
+		$Abilities/Attack/AttackCooldownTimer.stop()
+		var direction = (target.transform.origin - transform.origin).normalized() * Vector3(1,0,1)
 		
 		velocity = direction * SPEED
 
-		$AnimationTree.set("parameters/conditions/idle", target.global_position == self.global_position && is_on_floor())
-		$AnimationTree.set("parameters/conditions/walk", target.global_position != self.global_position && is_on_floor())
+	$AnimationTree.set("parameters/conditions/idle", close_targets.find(target) != -1 && is_on_floor())
+	$AnimationTree.set("parameters/conditions/walk", close_targets.find(target) == -1 && is_on_floor())
 
-		move_and_slide()
+	move_and_slide()
 
 func attack(target):
-	#target.on_hit() TODO: write an on_hit function for units that processes hits from other units
-	hit_player.emit()
+	velocity = Vector3.ZERO
+	target.on_hit()
+
+func _on_melee_range_body_entered(body: Player):
+	if body not in melee_targets && body != self:
+		melee_targets.append(body)
+
+func _on_melee_range_body_exited(body):
+	if body in melee_targets:
+		melee_targets.erase(body)
 
 func _on_close_range_body_entered(body: Player):
 	if body not in close_targets:
 		close_targets.append(body)
 
-func _on_close_range_body_exited(body: Player):
+func _on_close_range_body_exited(body):
 	if body in close_targets:
 		close_targets.erase(body)
+		
+func on_hit():
+	queue_free()
+
+func _on_attack_cooldown_timer_timeout():
+	attack(target)
