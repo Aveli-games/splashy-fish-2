8f12be661c1e7d7e54d01221dfe6ff4fb4033189
avelitj12 <avelitj12@gmail.com>
2024-04-22 12:59:14 -0600

    Add stamina bar

--- player.gd
+++ player.gd
@@ -63,6 +63,8 @@ func _physics_process(delta):
 	else:
 		stamina = clamp(stamina + stamina_regen_rate * delta, 0, 100)
 
+	$Abilities/Run/StaminaBarView/StaminaBar.value = stamina
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
